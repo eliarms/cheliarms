0eed969bc35ad7642d57b770044b991edc8a374a
Emmanuel Eliason Armstrong <geteliarms@gmail.com>
2020-12-31 01:25:59 +0700

    Added product and cart pages

---
+++ src/views/ProductDetailPage.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+<h1>..</h1>
+<div class="page-wrap">
+<div class="img-wrap">
+       
+<img v-bind:src="product.imageUrl"/>
+</div>
+
+<div id ="product-details">
+       
+<h1>{{product.name}}</h1>
+<h3 id="price">${{product.price}}</h3>
+<p>Average Rating: {{product.averageRating}}</p>
+<button id="add-to-cart">Add to Cart</button>
+<p>${{product.description}}</p>
+</div>
+
+
+
+
+
+
+</div>
+</div>
+</template>
+
+<script>
+import {products} from '../fake-data';
+export default {
+    name:'ProductDetailPage',
+    data(){
+      return {
+        product: products.find((p)=> p.id === this.$route.params.id) ,
+    }
+    }
+};
+</script>
+
+<style scoped>
+  #page-wrap {
+    margin-top: 16px;
+    padding: 16px;
+    max-width: 600px;
+  }
+
+  #img-wrap {
+    text-align: center;
+  }
+
+  img {
+    width: 400px;
+  }
+
+  #product-details {
+    padding: 16px;
+    position: relative;
+  }
+
+  #add-to-cart {
+    width: 100%;
+  }
+
+  #price {
+    position: absolute;
+    top: 24px;
+    right: 16px;
+  }
+</style>
